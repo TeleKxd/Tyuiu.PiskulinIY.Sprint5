df90576cbacea67cdf8397e504226d3e44263f7c
TeleKxd <piskulin.i@mail.ru>
2024-12-02 18:10:24 +0300

    Task4

--- Tyuiu.PiskulinIY.Sprint5.Task5.V6.Lib/DataService.cs
+++ Tyuiu.PiskulinIY.Sprint5.Task5.V6.Lib/DataService.cs
@@ -7,18 +7,18 @@ namespace Tyuiu.PiskulinIY.Sprint5.Task5.V6.Lib
         public double LoadFromDataFile(string path)
         {
             double res = 0;
-            double x = 0;
+            
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null) 
                 {
                     
-                    res = Math.Round(res + (Convert.ToDouble(line) / 20), 3);
+                    res = res + (Convert.ToDouble(line) / 20);
                 }
             }
 
-            return res;
+            return Math.Round(res, 3);
 
         }
     }
